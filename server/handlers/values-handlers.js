8f88b0852ba0d2c57c215c56405c8d93ab937eb5
Sanyam Gupta <sanyamgupta@Sanyams-MBP.home>
2024-08-10 18:56:57 -0700

    rough work

--- server/handlers/values-handlers.js
+++ server/handlers/values-handlers.js
@@ -2,34 +2,55 @@ const pgClient = require("../services/postgres-service");
 const redisClient = require("../services/redis-service");
 
 const getAllValues = async (req, res) => {
-  const values = await pgClient.query("SELECT * FROM values");
-  return res.status(200).json({
-    results: values.rows,
-  });
+  try {
+    // const values = await pgClient.query("SELECT * FROM values");
+    return res.status(200).json({
+      results: [],
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: e.message,
+    });
+  }
 };
 
 const getCurrentValues = async (req, res) => {
-  redisClient.hgetall("values", (err, values) => {
-    res.status(200).json({
-      results: values,
+  try {
+    // redisClient.hGetAll("values", (err, values) => {
+    //   res.status(200).json({
+    //     results: values,
+    //   });
+    // });
+    return res.status(200).json({
+      results: ["success"],
     });
-  });
+  } catch (e) {
+    res.status(500).json({
+      message: e.message,
+    });
+  }
 };
 
 const submitIndex = async (req, res) => {
-  const index = req.body.index;
-  const redisPublisher = redisClient.duplicate();
+  try {
+    const index = req.body.index;
+    const redisPublisher = redisClient.duplicate();
 
-  // Add the value to redis
-  redisClient.hset("values", index, -1);
-  redisPublisher.publish("insert", index);
+    // Add the value to redis
+    redisClient.hSet("values", index, -1);
+    redisPublisher.publish("insert", index);
 
-  // Add the value to postgres
-  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+    // Add the value to postgres
+    pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
 
-  res.status(201).json({
-    status: "IN_PROGRESS",
-  });
+    res.status(201).json({
+      status: "IN_PROGRESS",
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: e.message,
+    });
+  }
 };
 
 module.exports = {
